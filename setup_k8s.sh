8b3510a27df6258974b75a702b4e9c3f8c1e54c2
qiang.dai <qiang.dai@spirent.com>
2024-03-15 16:29:23 +0800

    fix small issues

--- setup_k8s.sh
+++ setup_k8s.sh
@@ -3,3 +3,5 @@
 ansible-playbook -i inventory.ini setup_k8s_precond.yaml
 ansible-playbook -i inventory.ini setup_crio.yaml
 ansible-playbook -i inventory.ini setup_k8s.yaml
+
+# ansible-playbook -i inventory.ini setup_worker.yaml -e master-ip="" token="" discovery-token-ca-cert-hash=""
